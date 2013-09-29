9de687fe900901ac9cf9ec87371e195556636a50
homoluden <asm@metaphysics-it.net>
2013-09-29 10:33:20 +0400

    IndexCtrl and Index view improved
    NG-REPEAT directive applied to render links at the top and left
    navigation panels

--- HTMLProxy/HTMLProxy/Scripts/ng-app/IndexCtrl.js
+++ HTMLProxy/HTMLProxy/Scripts/ng-app/IndexCtrl.js
@@ -1,6 +1,29 @@
 ﻿'use strict';
 
 var IndexCtrl = function($scope, $http) {
+    $scope.topLinks = [
+        { title: "люди", href: "/#", showIcon: false, showText: true },
+        { title: "сообщества", href: "/#", showIcon: false, showText: true },
+        { title: "игры", href: "/#", showIcon: false, showText: true },
+        { title: "музыка", href: "/#", showIcon: false, showText: true },
+        { iconClass: "glyphicon glyphicon-play", href: "/#", showIcon: true, showText: false },
+        { title: "помощь", href: "/#", showIcon: false, showText: true },
+        { title: "выйти", href: "/#", showIcon: false, showText: true }
+    ];
+
+    $scope.sideLinks = [
+        { title: "Моя Страница", href: "/#", showIcon: false, showText: true },
+        { title: "Мои Друзья", href: "/#", showIcon: false, showText: true },
+        { title: "Мои Фотографии", href: "/#", showIcon: false, showText: true },
+        { title: "Мои Видеозаписи", href: "/#", showIcon: false, showText: true },
+        { title: "Мои Аудиозаписи", href: "/#", showIcon: false, showText: true },
+        { title: "Мои Сообщения", href: "/#", showIcon: false, showText: true },
+        { title: "Мои Заметки", href: "/#", showIcon: false, showText: true },
+        { title: "Мои Группы", href: "/#", showIcon: false, showText: true },
+        { title: "Мои Новости", href: "/#", showIcon: false, showText: true },
+        { title: "Мои Закладки", href: "/#", showIcon: false, showText: true },
+        { title: "Мои Настройки", href: "/#", showIcon: false, showText: true }
+    ];
 
 }
 
